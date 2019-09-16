5c1819ba0ab08f12b51baa062b80a5b91d9c8b76
Jake Atkin <jake.a.atkin@gmail.com>
2019-09-16 13:41:59 +0100

    IDA-2132 #comment : Implemented state and css variables for the buttons

--- src/components/BaseISI.vue
+++ src/components/BaseISI.vue
@@ -1,24 +1,27 @@
 <template>
-  <div class="main-container">
+  <div
+    class="main-container"
+    v-bind:class="{ maximised: state == 'maximised',  minimised: state == 'minimised', closed: state == 'closed'}"
+  >
     <iframe name="isiContent" src="safetyInformation.html" v-on:load="loopSubheading()"></iframe>
     <div class="placeholder">
       <img :src="require('../../public/image-placeholder.jpg')" />
     </div>
-    <button class="Full-Prescribing-Information">Full Prescribing Information</button>
+    <button
+      class="Full-Prescribing-Information"
+      v-on:click="loadPrescribingInformation()"
+    >Full Prescribing Information</button>
 
-    <button v-on:click="expandUp()" class="expand">Expand</button>
+    <button v-on:click="state = 'maximised'" class="expand" ref="expand">Expand</button>
+    <button v-on:click="state = 'minimised'" class="collapse" ref="collapse">Collapse</button>
 
-    <button>Collapse</button>"
-    <div v-on:click="collapse()" class="collapse"></div>
-    <!--  v-on:click="minimised" : state = "minimised" class='collapse' -->
     <div class="subheadings">
       <div v-for="label in labels">
-        <a v-bind:href="'safetyInformation.html#' + label" v-bind:key target="isiContent">{{label}}</a>
+        <a v-bind:href="'safetyInformation.html#' + label">{{label}}</a>
       </div>
     </div>
   </div>
 </template>
-
 
 <script>
 export default {
@@ -26,44 +29,20 @@ export default {
   data() {
     return {
       labels: [],
-      state: ""
+      state: "minimised"
     };
   },
-  // {maximised : state="maximised", minimised: state="minimised", closed: state="closed"}
+
   methods: {
-    expandUp() {
-      const container = document.querySelector("div .main-container");
-      container.classList.add("maximised");
-      this.state = "maximised";
-      container.classList.remove("minimised");
-
-      const expandBTN = document.querySelector(".expand");
-      expandBTN.classList.add("hidden");
-
-      const collapseBTN = document.querySelector(".collapse");
-      collapseBTN.classList.add("show");
-
-      const grid = document.querySelector(".collapse");
-      let collapse = "<button class='collapse'>Collapse</button>";
-      grid.innerHTML = collapse;
-      grid.classList.add("show");
+    closed() {
+      state = "closed";
     },
 
-    collapse() {
-      const container = document.querySelector("div .main-container");
-      container.classList.add("minimised");
-      container.classList.remove("maximised");
-      this.state = "minimised";
-
-      const expandBTN = document.querySelector(".expand");
-      expandBTN.classList.remove("hidden");
-
-      const collapseBTN = document.querySelector(".collapse");
-      collapseBTN.classList.remove("show");
+    loadPrescribingInformation() {
+      console.log("Axios will be here!");
     },
 
     loopSubheading() {
-      console.log("Fired!");
       const subheadings = document
         .querySelector("iframe")
         .contentWindow.document.querySelectorAll("h3");
@@ -86,6 +65,11 @@ iframe {
   font-family: arial;
 }
 
+img {
+  position: sticky;
+  top: 50px;
+  z-index: 10;
+}
 .main-container {
   background-color: transparent;
   color: darkblue;
@@ -114,25 +98,11 @@ iframe {
   display: block;
 }
 
-.collapse {
-  font-size: 24px;
-  position: absolute;
-  top: -25px;
-  right: 10px;
-  background: blue;
-  color: white;
-  font-size: 16px;
-}
-
 .placeholder {
   top: 0px;
   left: -180px;
   position: absolute;
   border-left: none;
-}
-
-.collapse {
-  display: none;
 }
 
 .Full-Prescribing-Information {
@@ -153,29 +123,42 @@ iframe {
   font-size: 16px;
 }
 
+.collapse {
+  position: absolute;
+  right: 80px;
+  bottom: 122px;
+  background-color: blue;
+  color: white;
+  font-size: 16px;
+}
+
 /* Class Styles for State */
 
 .maximised {
   top: 50px;
 }
 
-/* looking for multiple children  */
-.expanded .maximised {
+.maximised .expand {
+  display: none;
+}
+
+.maximised .collapse {
+  display: unset;
 }
 
 .minimised {
   top: 850px;
 }
 
+.minimised .collapse {
+  display: none;
+}
+
 .closed {
   top: 950px;
 }
 
-.hidden {
+.closed .collapse {
   display: none;
-}
-
-.show {
-  display: unset;
 }
 </style>
